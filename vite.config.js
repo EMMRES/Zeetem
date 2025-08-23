689dfef681563f2283c9ef1af7251fcc0ca87274
Adeusi emmanuel <123315114+EMMRES@users.noreply.github.com>
2025-08-23 21:07:13 +0100

    Updates

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: "Zeetem/", // Use relative path for Namecheap or cPanel hosting
-  plugins: [react()],
-});
